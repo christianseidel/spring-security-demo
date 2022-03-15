6da78d15fe321fdae5435e1877cda894faa8e851
Christian Seidel <christianseidel@LeGrandMac.fritz.box>
2022-03-15 12:17:33 +0100

    hashing fine

--- src/main/java/de/neuefische/securitydemo/UserController.java
+++ src/main/java/de/neuefische/securitydemo/UserController.java
@@ -1,6 +1,7 @@
 package de.neuefische.securitydemo;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping
     public UserDocument createUser (@RequestBody UserDocument user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userService.createUser(user);
     }
 }
